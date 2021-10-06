881c86470cbeeaecaf7700ed85b7c662f8084b6e
Gary Leija <gleija@seek.com.au>
2021-10-06 16:29:09 +1100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RotatingView from './components/RotatingView';
+
+const App: React.FunctionComponent = () => {
+  return <RotatingView />;
+};
+
+export default App;
---
+++ src/components/RotatingView.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const RotatingView: React.FunctionComponent = () => <></>;
+
+export default RotatingView;
